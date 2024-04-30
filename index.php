7ff528454dad4cf63ba2c423178bbea88bb9b050
João Pedro Marques padovan <jpmpadovan@gmail.com>
2024-04-30 10:14:52 -0300

    Atualização para mostrar os aniversariantes de admissão na empresa

--- index.php
+++ index.php
@@ -19,19 +19,25 @@
         foreach ($colaboradoresValores as $row) {
             // Verifica se o segundo elemento do array existe e se o quarto elemento não existe
             if (isset($row[1]) && !isset($row[3])) {
-                $aniverColaborador = $row[1];
+                $nasciColaborador = $row[1];
+                $aniverAdmissao = $row[2];
                 
                 // Converte a data de nascimento para um objeto DateTime
-                $dataNascimento = DateTime::createFromFormat('d/m/Y', $aniverColaborador);
+                $dataNascimento = DateTime::createFromFormat('d/m/Y', $nasciColaborador);
+                $dataAdmissao = DateTime::createFromFormat('d/m/Y', $aniverAdmissao);
                 
                 $diferenca = $dataNascimento->diff($dataAtual);
                 $idade = $diferenca->y;
+                $diferencaAdmissao = $dataAdmissao->diff($dataAtual);
+                $anosEmpresa = $diferencaAdmissao->y;
                 
                 // Define o ano da data de nascimento como o ano atual
                 $dataNascimento->setDate($dataAtual->format('Y'), $dataNascimento->format('m'), $dataNascimento->format('d'));
+                $dataAdmissao->setDate($dataAtual->format('Y'), $dataAdmissao->format('m'), $dataAdmissao->format('d'));
                 
                 // Calcula a diferença entre a data de nascimento e a data atual
                 $diff = $dataAtual->diff($dataNascimento);
+                $diffAdmissao = $dataAtual->diff($dataAdmissao);
                 
                 // Verifica se o aniversário já passou, é hoje ou ainda está por vir
                 if ($diff->format('%R%a') < 0) {
@@ -41,6 +47,16 @@
                 } else {
                     $idade = $idade + 1;
                     echo 'O aniversário de ' . $row[0] . ' será em ' . $diff->format('%a') . ' dias. E ele(a) completará ' . $idade . ' anos.<br>';
+                }
+                
+
+                if ($diffAdmissao->format('%R%a') < 0) {
+                    echo 'O aniversario de empresa de ' . $row[0] . ' já foi. Ele(a) completou ' . $anosEmpresa . ' anos de empresa.<br>';
+                } elseif ($diffAdmissao->format('%R%a') == 0) {
+                    echo 'O aniversario de empresa de ' . $row[0] . ' é hoje! Que está completando ' . $anosEmpresa .  ' anos de empresa.<br>';
+                } else {
+                    $anosEmpresa = $anosEmpresa + 1;
+                    echo 'O aniversario de ' . $row[0] . ' será esse ano ainda! Ele(a) estará completando ' . $anosEmpresa . ' anos de empresa.<br>';
                 }
             }
         }
